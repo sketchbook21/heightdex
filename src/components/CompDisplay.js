d0f9ac518ff7d2a0ca1c9c970afe0506f681e2ee
sketchbook21 <gilbert.hsu2@gmail.com>
2020-04-14 13:31:08 -0400

    wip

---
+++ src/components/CompDisplay.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const CompDisplay = () => {
+  return <Container className="cont">Pokemon Images Here</Container>;
+};
+
+export default CompDisplay;
