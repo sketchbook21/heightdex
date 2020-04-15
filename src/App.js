fa82ed27a759c299ba526939842002b0fabb087e
sketchbook21 <gilbert.hsu2@gmail.com>
2020-04-14 20:37:19 -0400

    logic completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import React from "react";
-import { Container, Row, Col, InputGroup, FormControl } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Container, Row, Spinner } from "react-bootstrap";
 import "./App.scss";
 import Search from "./components/Search";
 import Footer from "./components/Footer";
 import CompDisplay from "./components/CompDisplay";
 
-function App() {
-  return (
-    <>
-      <Container className="w-50">
+const App = () => {
+  const [display, setDisplay] = useState(false);
+  const [options, setOptions] = useState([]);
+  const [search, setSearch] = useState("");
+  const [loading, isLoading] = useState(false);
+
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  const fetchPokemon = async () => {
+    isLoading(true);
+    const pokemon = [];
+    const promises = new Array(807)
+      .fill()
+      .map((value, index) =>
+        fetch(`https://pokeapi.co/api/v2/pokemon/${index + 1}/`)
+      );
+
+    await Promise.all(promises).then((pokemonArr) => {
+      return pokemonArr.map((response) =>
+        response
+          .json()
+          .then(({ name, sprites: { front_default: sprite }, height }) => {
+            return pokemon.push({ name, sprite, height });
+          })
+      );
+    });
+    setOptions(pokemon);
+    isLoading(false);
+  };
+
+  const setPokedex = (poke) => {
+    setSearch(poke);
+    setDisplay(false);
+  };
+
+  const handleInputChange = (pokemon) => {
+    setSearch(pokemon);
+    if (search.length > 1) {
+      setDisplay(true);
+    } else {
+      setDisplay(false);
+    }
+  };
+
+  let content;
+  if (loading) {
+    content = (
+      <Container className="h-100">
+        <Spinner animation="border" variant="warning" />
+      </Container>
+    );
+  } else {
+    content = (
+      <Container>
         <Row>
-          <Search />
+          <Search
+            display={display}
+            setDisplay={setDisplay}
+            options={options}
+            search={search}
+            setPokedex={setPokedex}
+            handleInputChange={handleInputChange}
+          />
         </Row>
         <Row>
           <CompDisplay />
         </Row>
       </Container>
+    );
+  }
+
+  return (
+    <>
+      {content}
       <Footer />
     </>
   );
-}
+};
 
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,43 @@
 import React from "react";
-import { InputGroup, FormControl } from "react-bootstrap";
+import { Container, InputGroup, FormControl, Spinner } from "react-bootstrap";
 
-const Search = () => {
+const Search = ({
+  display,
+  setDisplay,
+  options,
+  search,
+  setPokedex,
+  handleInputChange,
+}) => {
   return (
-    <InputGroup className="mt-5 mb-3">
-      <FormControl placeholder="Search Pokemon" id="search" />
-    </InputGroup>
+    <>
+      <InputGroup className="mt-5 mb-3">
+        <FormControl
+          placeholder="Search Pokemon"
+          id="search"
+          value={search}
+          onChange={(event) => handleInputChange(event.target.value)}
+        />
+      </InputGroup>
+      {display && (
+        <div className="options-container w-100">
+          {options
+            .filter(({ name }) => name.indexOf(search.toLowerCase()) > -1)
+            .map((value, index) => {
+              return (
+                <div
+                  className="option"
+                  key={index}
+                  onClick={() => setPokedex(value.name)}
+                >
+                  <span>{value.name}</span>
+                  <img src={value.sprite} alt="pokemon" />
+                </div>
+              );
+            })}
+        </div>
+      )}
+    </>
   );
 };
 
