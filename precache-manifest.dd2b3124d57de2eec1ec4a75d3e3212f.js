35c179337b60fd71eb8eaa73e56adfb0e6b94950
Gilbert Hsu (via Travis CI) <45667486+sketchbook21@users.noreply.github.com>
2020-04-18 03:53:17 +0000

    Deploy sketchbook21/heightdex to github.com/sketchbook21/heightdex.git:gh-pages

---
+++ precache-manifest.dd2b3124d57de2eec1ec4a75d3e3212f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce51d28aa16f955c0d2a01f295f596b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd2de69daf1ea5d8298a",
+    "url": "/static/css/main.994d5c32.chunk.css"
+  },
+  {
+    "revision": "78c4446e2700100150ff",
+    "url": "/static/js/2.68115622.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.68115622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd2de69daf1ea5d8298a",
+    "url": "/static/js/main.2284b75e.chunk.js"
+  },
+  {
+    "revision": "223cb8fd72aac2915e3a",
+    "url": "/static/js/runtime-main.07266b0f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c82446899ef4ba77c94deaea0c57f43.js"
+  "/precache-manifest.dd2b3124d57de2eec1ec4a75d3e3212f.js"
 );
 
 self.addEventListener('message', (event) => {
